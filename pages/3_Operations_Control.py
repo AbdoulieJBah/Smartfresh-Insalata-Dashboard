3a718fb0bac26e8e5879ddae52018d08e1b89286
AbdoulieJBah <155037012+AbdoulieJBah@users.noreply.github.com>
2026-05-03 20:10:48 +0200

    Update 3_Operations_Control.py

--- pages/3_Operations_Control.py
+++ pages/3_Operations_Control.py
@@ -5,10 +5,11 @@ import plotly.express as px
 from utils import inject_css
 from data_utils import load_data
 from auth_utils import require_role, get_current_user
+from utils import setup_page
 
 require_role(["Admin", "Manager", "Operations", "Logistics"])
 
-st.set_page_config(page_title="Operations Control", layout="wide")
+setup_page("Operations Control")
 
 inject_css()
 
